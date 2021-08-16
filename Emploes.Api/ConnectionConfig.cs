33f1304024bf22a04bcb5a7fb161fac483b9995b
BatonGamedesigner <37902684+BatonGamedesigner@users.noreply.github.com>
2021-08-16 15:29:16 +0300

    init

---
+++ Emploes.Api/ConnectionConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emploes.Api
+{
+    public class ConnectionConfig
+    {
+        public string DapperConnection { get; set; }
+    }
+}
